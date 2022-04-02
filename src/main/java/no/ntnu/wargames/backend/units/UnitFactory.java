2192a95de4fe9452d52ee275dd3b5849320f8a75
KjetilLaptop <kjetikin@stud.ntnu.no>
2022-04-02 21:21:18 +0200

    add: factory method that return a list of units

--- src/main/java/no/ntnu/wargames/backend/units/UnitFactory.java
+++ src/main/java/no/ntnu/wargames/backend/units/UnitFactory.java
@@ -1,5 +1,8 @@
 package no.ntnu.wargames.backend.units;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This is the unit factory class.
  * This class uses the "factory" design pattern.
@@ -9,6 +12,10 @@ package no.ntnu.wargames.backend.units;
  */
 
 public class UnitFactory {
+
+    /*Unit types*/
+    public static final List<String> UNIT_TYPES = List.of("Ranged","Infantry","Commander","Cavalry");
+
 
     /**
      * Factory method that returns a new unit.
@@ -27,5 +34,35 @@ public class UnitFactory {
             case "Cavalry" -> new CavalryUnit(unitName, unitHealth);
             default -> null;
         };
+    }
+
+
+    /**
+     * Create x amount of units, and returns them in a list.
+     * All the unit created have the same type,name and health
+     *
+     * @param amount the amount of units created
+     * @param unitType the unit type of the units
+     * @param unitName the name of each unit
+     * @param unitHealth the health of each unit
+     * @return returns a list of units
+     * @throws IllegalArgumentException throws exception if a given argument is wrong. Check the exception message.
+     */
+
+    public List<Unit> createListOfUnit(int amount, String unitType,String unitName, int unitHealth) throws IllegalArgumentException{
+        List<Unit> unitList = new ArrayList<>();
+
+        /*Check if the given parameters are correct, then throws correct exception*/
+
+        if(!(UNIT_TYPES.contains(unitType))){throw new IllegalArgumentException("Unknown unit type given");}
+        if(amount <= 0){throw new IllegalArgumentException("Amount was negative");}
+        if(unitHealth <= 0){throw new IllegalArgumentException("Unit health was negative");}
+        if(unitName.isEmpty()){throw new IllegalArgumentException("No name given");}
+
+        /* Add x amount of same type of units to the return list*/
+        for(int i = 0; i<amount; i++){
+            unitList.add(createUnit(unitType,unitName,unitHealth));
+        }
+        return unitList;
     }
 }
