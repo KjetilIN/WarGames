89854801e920b4296876b2b72873de0ed9447ce8
KjetilMainPc <kjetilindrehus@gmail.com>
2022-04-04 14:07:09 +0200

    feat: graph of units in the army during simulation

--- src/main/java/no/ntnu/wargames/frontend/gui/controllers/simulationController.java
+++ src/main/java/no/ntnu/wargames/frontend/gui/controllers/simulationController.java
@@ -4,6 +4,10 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -18,14 +22,16 @@ import java.util.concurrent.TimeUnit;
 
 public class simulationController implements Initializable {
 
-    /*Methods for the simulation*/
+    /*Fields for the simulation*/
     private Battle battle;
     private Army army1;
     private Army army2;
 
 
+
     @FXML
-    private GridPane world;
+    private AreaChart<String,Number> unitGraph;
+
 
     @FXML
     private TextArea battleLog;
@@ -39,12 +45,29 @@ public class simulationController implements Initializable {
 
     public void logBattle(){
 
+        unitGraph.getData().clear();
+
+        //defining units in each army s
+        XYChart.Series<String , Number> army1Units = new XYChart.Series<>();
+        XYChart.Series<String, Number> army2Units = new XYChart.Series<>();
+        army1Units.setName("Unit in " + this.army1.getName()+"'s army");
+        army2Units.setName("Units in " + this.army2.getName()+"'s army");
+
+
+
         String textLog = "";
+        int attackCount = 1;
         while(army1.hasUnits() && army2.hasUnits()){
             textLog += battle.simulateStep() +"\n";
+            army1Units.getData().add(new XYChart.Data<>(String.valueOf(attackCount),this.army1.getAllUnits().size()));
+            army2Units.getData().add(new XYChart.Data<>(String.valueOf(attackCount),this.army2.getAllUnits().size()));
             battleLog.setText(textLog);
+            attackCount++;
         }
 
+        //Set information of the chart
+        this.unitGraph.getData().addAll(army1Units,army2Units);
+        this.unitGraph.setCreateSymbols(false);
     }
     @FXML
     public void onSimulate(){
@@ -55,5 +78,10 @@ public class simulationController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.unitGraph.setTitle("Units in army");
+        this.unitGraph.getYAxis().setLabel("Unit(s)");
+        this.unitGraph.getXAxis().setLabel("Attack(s)");
+        this.unitGraph.getXAxis().setTickLabelsVisible(false);
+        this.unitGraph.setCreateSymbols(false);
     }
 }
