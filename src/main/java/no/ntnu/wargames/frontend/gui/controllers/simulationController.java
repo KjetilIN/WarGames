4d4b2a2e1d61ba4ac4dacb02607f80cda0873cf5
KjetilMainPc <kjetilindrehus@gmail.com>
2022-04-05 15:59:31 +0200

    refactor: made battle happen in a timeline

--- src/main/java/no/ntnu/wargames/frontend/gui/controllers/simulationController.java
+++ src/main/java/no/ntnu/wargames/frontend/gui/controllers/simulationController.java
@@ -2,6 +2,7 @@ package no.ntnu.wargames.frontend.gui.controllers;
 
 
 
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
@@ -10,22 +11,37 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-
 import javafx.util.Duration;
 import no.ntnu.wargames.backend.Battle;
 import no.ntnu.wargames.backend.units.Army;
-
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class simulationController implements Initializable {
+
 
     /*Fields for the simulation*/
     private Battle battle;
     private Army army1;
     private Army army2;
+    private Timeline timeline;
+    private int simulationStep;
+
+    private XYChart.Series<String , Number> army1UnitsSeries;
+    private XYChart.Series<String, Number> army2UnitsSeries;
+
+    private XYChart.Series<String,Number> army1SumHealth;
+    private XYChart.Series<String,Number> army2SumHealth;
+
+    /*Info labels */
+    @FXML
+    private Label unitCountArmy1;
+
+    @FXML
+    private Label unitCountArmy2;
 
 
     @FXML
@@ -44,34 +60,21 @@ public class simulationController implements Initializable {
 
     }
 
-    public void logBattle(){
+    public void setupGraphsBeforeSim(){
 
         unitGraph.getData().clear();
         healthGraph.getData().clear();
 
         //defining units in each army s
-        XYChart.Series<String , Number> army1UnitsSeries = new XYChart.Series<>();
-        XYChart.Series<String, Number> army2UnitsSeries = new XYChart.Series<>();
+        army1UnitsSeries = new XYChart.Series<>();
+        army2UnitsSeries = new XYChart.Series<>();
         army1UnitsSeries.setName("Unit in " + this.army1.getName()+"'s army");
         army2UnitsSeries.setName("Units in " + this.army2.getName()+"'s army");
 
-        XYChart.Series<String,Number> army1SumHealth = new XYChart.Series<>();
-        XYChart.Series<String,Number> army2SumHealth = new XYChart.Series<>();
+        army1SumHealth = new XYChart.Series<>();
+        army2SumHealth = new XYChart.Series<>();
         army1SumHealth.setName("HP sum of " + this.army1.getName()+"'s army");
         army2SumHealth.setName("HP sum of " + this.army2.getName()+"'s army");
-
-        String textLog = "";
-        int attackCount = 1;
-        while(army1.hasUnits() && army2.hasUnits()){
-            textLog += battle.simulateStep() +"\n";
-            army1UnitsSeries.getData().add(new XYChart.Data<>(String.valueOf(attackCount),this.army1.getAllUnits().size()));
-            army2UnitsSeries.getData().add(new XYChart.Data<>(String.valueOf(attackCount),this.army2.getAllUnits().size()));
-
-            army1SumHealth.getData().add(new XYChart.Data<>(String.valueOf(attackCount),this.army1.getAllUnitHealthSum()));
-            army2SumHealth.getData().add(new XYChart.Data<>(String.valueOf(attackCount),this.army2.getAllUnitHealthSum()));
-            battleLog.setText(textLog);
-            attackCount++;
-        }
 
         //Set information of the chart
         this.unitGraph.getData().addAll(army1UnitsSeries,army2UnitsSeries);
@@ -80,6 +83,23 @@ public class simulationController implements Initializable {
         this.healthGraph.setCreateSymbols(false);
 
 
+
+    }
+
+    public void simStep(ActionEvent event){
+        String textLog = "";
+        if (army1.hasUnits() && army2.hasUnits()){
+            textLog += battle.simulateStep() +"\n";
+            unitCountArmy1.setText(String.valueOf(army1.getAllUnits().size()));
+            unitCountArmy2.setText(String.valueOf(army2.getAllUnits().size()));
+            army1UnitsSeries.getData().add(new XYChart.Data<>(String.valueOf(simulationStep),this.army1.getAllUnits().size()));
+            army2UnitsSeries.getData().add(new XYChart.Data<>(String.valueOf(simulationStep),this.army2.getAllUnits().size()));
+
+            army1SumHealth.getData().add(new XYChart.Data<>(String.valueOf(simulationStep),this.army1.getAllUnitHealthSum()));
+            army2SumHealth.getData().add(new XYChart.Data<>(String.valueOf(simulationStep),this.army2.getAllUnitHealthSum()));
+            battleLog.setText(battleLog.getText()+ textLog);
+            simulationStep++;
+        }
 
     }
 
@@ -102,14 +122,17 @@ public class simulationController implements Initializable {
 
     @FXML
     public void onSimulate(){
-        logBattle();
-        //notifyWinner();
+        setupGraphsBeforeSim();
+        timeline = new Timeline(new KeyFrame(Duration.millis(20),this::simStep));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
 
     }
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.simulationStep = 0;
         //Setting style for unit graph
         this.unitGraph.setTitle("Units in army");
         this.unitGraph.getYAxis().setLabel("Unit(s)");
