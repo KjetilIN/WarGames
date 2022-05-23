e036d469a5b91bc6e7b9234096291da1ae479e2d
KjetilLaptop <kjetikin@stud.ntnu.no>
2022-05-23 16:25:28 +0200

    fix: try to fix maven errors

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,16 +4,10 @@ module no.ntnu.wargames{
     requires javafx.controls;
 
     opens no.ntnu.wargames.frontend.gui.controllers to javafx.fxml, javafx.controls;
-    exports no.ntnu.wargames.frontend.gui.controllers;
     opens no.ntnu.wargames.backend.units to javafx.fxml, javafx.controls;
-    exports no.ntnu.wargames.backend.units;
-    exports no.ntnu.wargames;
-    exports no.ntnu.wargames.backend;
     opens no.ntnu.wargames to javafx.fxml, javafx.controls;
-    exports no.ntnu.wargames.backend.designPattern;
     opens no.ntnu.wargames.backend.designPattern to javafx.controls, javafx.fxml;
     opens no.ntnu.wargames.backend.file;
-    exports no.ntnu.wargames.backend.file;
     opens no.ntnu.wargames.frontend.gui.dialog.complexDialog to javafx.fxml, javafx.controls;
     opens no.ntnu.wargames.frontend.gui.dialog.simpleDialog to javafx.fxml, javafx.controls;
     opens no.ntnu.wargames.frontend.gui.dialog to javafx.fxml, javafx.controls;
@@ -21,5 +15,11 @@ module no.ntnu.wargames{
     exports no.ntnu.wargames.frontend.gui.dialog.complexDialog;
     exports no.ntnu.wargames.frontend.gui.dialog.simpleDialog;
     exports no.ntnu.wargames.frontend.gui.dialog;
+    exports no.ntnu.wargames.backend;
+    exports no.ntnu.wargames.backend.designPattern;
+    exports no.ntnu.wargames.backend.file;
+    exports no.ntnu.wargames.frontend.gui.controllers;
+    exports no.ntnu.wargames.backend.units;
+    exports no.ntnu.wargames;
 
 }
